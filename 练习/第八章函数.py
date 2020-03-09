d378f2fe67652daedf0d3f3c189514731ce89a9b
tiajun163 <tiajun163@163.com>
2020-03-09 15:35:22 +0800

    Default

--- 练习/第八章函数.py
+++ 练习/第八章函数.py
@@ -6,6 +6,13 @@ def favorite_book(titles):
 favorite_book(input("请输入你的书名："))
 #传多个参数
 def fbook(name,price):
-    print(name.tiile+'书名')
+    print(name.title()+'书名')
     print(price+'价格')
-# fbook('yj','25')
+fbook('yj','25')
+fbook('ss','89')
+fbook(input('请输入书名'),input('输入价格'))
+#关键字实参
+def describe_pet(animal_type,pet_name):
+    print("动物类型;"+animal_type)
+    print("动物名字"+pet_name.title())
+describe_pet(animal_type='猫',pet_name='jik')
