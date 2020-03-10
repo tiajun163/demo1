e68b7494b1b4fc94b67d2841ae2924a74c699d82
tiajun163 <tiajun163@163.com>
2020-03-10 20:26:52 +0800

    Default

--- 第九章类/dog_test.py
+++ 第九章类/dog_test.py
@@ -7,9 +7,12 @@ class Dog():
     def sit( self ):
         print(self.name.title()+'issitting')
     def roll_over( self ):
-        print(self.age.title()+'rolled over')
-my_dog=Dog('wille',6)
+        print(str(self.age)+':\trolled over')
+my_dog=Dog('wille',6)#传参数
 your_dog=Dog('luy',10)
 print('my dogstr nameis'+my_dog.name.title()+'.')
-print('my dog is age'+str(my_dog.age)+'years old')
-my_dog.sit()
+print('my dog is age:'+str(my_dog.age)+':\tyears old')
+my_dog.sit()#调用类中的方法
+print('your_dog惊声尖叫：'+your_dog.name.title())
+print('your_dog 多大了：'+str(your_dog.age))
+your_dog.roll_over()
---
+++ 第九章类/practice.py
@@ -0,0 +1,8 @@
+''''练习9-1'''
+class Restaurant():
+    def __init__(self,res_name,cuis_type):
+        #初始化属性
+        self.res_name=res_name
+        self.cuis_name=cuis_type
+    def des_res( self ):
+
