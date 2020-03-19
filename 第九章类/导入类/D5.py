059772ad07d0880b0fdf6216969f584db74a0619
tiajun163 <tiajun163@163.com>
2020-03-19 11:34:38 +0800

    Default

---
+++ 第九章类/导入类/D5.py
@@ -0,0 +1,14 @@
+# ''''’9-6练习'''
+# class Res():
+#     def __init__(self,res_name,res_type):
+#         ''''初始化类的属性'''
+#         self.res_name=res_name
+#         self.res_type=res_type
+#         self.res_number=0
+#     def des_res( self ):
+#         ''''调用描述冰淇淋的方法'''
+#         msg=self.res_name+'是一家特立独行的店'+'也是一家'+self.res_type+\
+#             '类型的店'
+#         # return  msg
+#         print(msg)
+#     def
