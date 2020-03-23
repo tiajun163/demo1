d83b9705a3d3f2d957febb5f7ccaa74b6da515db
tiajun163 <tiajun163@163.com>
2020-03-23 14:50:57 +0800

    练习10-3

--- 第十章文件和异常/s1.py
+++ 第十章文件和异常/s1.py
@@ -1,4 +1,8 @@
 #练习10-3访客
 name='s.txt'
 with open(name,'r+') as file_obj:
-    file_obj.write(input('请输入你的名字！'))
+    file_obj.write(input('请输入你的名字！'+'\n'))
+names=input("请输入你的名字")
+fiel='ss.txt'
+with open(fiel,'a') as fie_obj:
+    fie_obj.write(names+'\n')
