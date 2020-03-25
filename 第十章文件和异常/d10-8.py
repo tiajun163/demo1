5996f051af0b55966355a60486b067d96594295c
tiajun163 <tiajun163@163.com>
2020-03-25 09:03:55 +0800

    练习10-3

--- 第十章文件和异常/d10-8.py
+++ 第十章文件和异常/d10-8.py
@@ -1,11 +1,13 @@
 #练习10-8
-fielname=['ds.txt','ds1.txt']
+fielname=['ds.txt','ds1.txt','re1.txt']
 for name in fielname:#遍历列表
     print('文件包含内容：'+name)
     try:#处理异常
-        with open(name)as f:
+        with open(name,'r',encoding='UTF-8')as f:#rb,转为二进制
            con= f.read()#读取列表文件
-           print(con)
+           # print(con)
     except FileNotFoundError:
-        print(name+"不存在")
-        
+        # print(name+"不存在")
+        pass#不处理，直接沉默不返回
+    else:
+        print(con)
