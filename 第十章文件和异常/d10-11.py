140ddaee459d7b68b4abcbacf2c22d0c5032e8fb
tiajun163 <tiajun163@163.com>
2020-03-26 09:47:34 +0800

    练习10-3

--- 第十章文件和异常/d10-11.py
+++ 第十章文件和异常/d10-11.py
@@ -1,8 +1,20 @@
 #练习10-11
 import  json
-number=input("输入你喜欢的数字：\n")
+# number=input("输入你喜欢的数字：\n")
 funber='numbers.json'
-with open(funber,'w')as f:
-        json.dump(number,f)
-        print('你喜欢的数字是：\n'+number)
-        
+# with open(funber,'w')as f:
+#         json.dump(number,f)
+#         print('你喜欢的数字是：\n'+number)
+#练习10-12
+try:
+    with open(funber)as f:
+        con=json.load(f)
+except FileNotFoundError:
+    number = input ("输入你喜欢的数字：\n")
+    with open(funber,'w')as f:
+        json.dump(funber,f)
+        print('你登记的数字：'+number)
+else:
+    print(
+        '你喜欢的数字已登记为：'+con
+    )
