120e525a8c3eb7096e4395e3ca88dbd008daa725
tiajun163 <tiajun163@163.com>
2020-04-01 10:22:16 +0800

    练习10-3

--- 第十一章测试代码/testclass/testshow.py
+++ 第十一章测试代码/testclass/testshow.py
@@ -5,6 +5,24 @@ from 第十一章测试代码.testclass.testclassdemo import AnonySurvey
 
 
 class TestAnouysurvey(unittest.TestCase):
+    #使用setUp()方法
+    def setUp(self):
+        #创建一个调查对象和一组答案，供使用的测试方法使用
+        question='你学的什么语音'
+        self.my_survey=AnonySurvey(question)#创建一个调查对象
+        self.responses=['英语','法语','汉语']#创建一组答案
+    def test_stors( self ):
+        #单个测试答案
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0],self.my_survey.responses)
+    def test_stores( self ):
+        #多个测试答案
+        for re in self.responses:#遍历列表
+            self.my_survey.store_response(re)
+        for re in self.responses:
+            self.assertIn(re,self.my_survey.responses)#断言答案是否存在列表中
+        
+        
     #测试单个答案并存储
     def test_stor( self ):
         question='你学什么语言'
